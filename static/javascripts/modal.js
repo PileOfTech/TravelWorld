87dbe722872097b1b9a55902080a32b456bf3160
ArioShaman <ariosha6@gmail.com>
2018-05-11 02:50:52 +0300

    Jungle adaptive

---
+++ static/javascripts/modal.js
@@ -0,0 +1,121 @@
+$('.globus').click(function(){
+  $('.big-logo').toggleClass('top-logo');
+  $('.line').toggleClass('top-line');
+});
+
+
+/*Modal controll for sea-slide*/
+$('.tour').click(function(){
+  $('body').css('overflow', 'hidden'); 
+  var theme = $(this).attr('theme');
+  if(theme == "bali"){
+    $('.sea-modal-window-02').addClass('open');
+    $('.bali').addClass('active');
+  }
+  if(theme == "LA"){
+    $('.sea-modal-window-02').addClass('open');
+    $('.LA').addClass('active');
+  }
+  if(theme == "hawaii"){
+    $('.sea-modal-window-01').addClass('open');
+    $('.hawaii').addClass('active');
+  }
+  if(theme == "PL"){
+    $('.sea-modal-window-01').addClass('open');
+    $('.point-lobos').addClass('active');
+  }                          
+});
+
+/*Modal controll for hicing*/
+$('.name-tour').click(function(){
+  $('.hic-modal-window').addClass('open');
+  $('body').css('overflow', 'hidden');
+  var theme = $(this).attr('theme');
+  if(theme == "red"){
+    $('.red-theme').addClass('active');
+    slick();
+  }
+  if(theme == "yellow"){
+    $('.yellow-theme').addClass('active');
+  }      
+  if(theme == "green"){
+    $('.green-theme').addClass('active');
+  }        
+});
+
+
+var skiData = [
+  {
+    "title": "Kirchdorf",
+    "text": "Hit the slopes running (or should we say, boarding) on this awesome ski and snowboard trip in the Austrian Alps. Make your own way to Kirchdorf in the Austrian Tyrol region, then tailor your equipment hire and lessons upon arrival. You'll gain access to world-class runs that satisfy beginners",
+    "cover": "cov-01.jpg",
+  },
+  {
+    "title": "Queenstown",
+    "text": "There’s one word to describe the sights you’ll see in New Zealand: dramatic. Whether it’s the mirror-like lakes, soaring mountains or the wildlife of Queenstown, there’s nowhere quite like the Land of the Long White Cloud. Strap on those skis, take to the slopes and enjoy the trip.",
+    "cover": "cov-02.jpg",    
+  },
+  {
+    "title": "Japanese",
+    "text": "Hit the powder and max out on moguls for an entire week. Make your way to the nearby ski resorts and apres-ski your way from green to black runs. With free time to make this trip your own, visit snow monkeys, a sake brewery, a temple tour, and plenty of Japanese noms thrown in.",
+    "cover": "cov-03.jpg",    
+  
+  }
+]
+$('.ski-but').click(function(){
+  $('.ski-modal-window').addClass('open');
+  $('body').css('overflow', 'hidden');
+  var theme = $(this).attr('theme');
+  if(theme == 'but-01'){
+    $('.ski-title').text(skiData[0]["title"]);
+    $('.ski-text').text(skiData[0]["text"]);
+    $('.ski-cover').css("background-image", "url('static/css/images/"+skiData[0]["cover"]+"')");
+  }
+  if(theme == 'but-02'){
+    $('.ski-title').text(skiData[1]["title"]);
+    $('.ski-text').text(skiData[1]["text"]);
+    $('.ski-cover').css("background-image", "url('static/css/images/"+skiData[1]["cover"]+"')");
+  }
+  if(theme == 'but-03'){
+    $('.ski-title').text(skiData[2]["title"]);
+    $('.ski-text').text(skiData[2]["text"]);
+    $('.ski-cover').css("background-image", "url('static/css/images/"+skiData[2]["cover"]+"')");
+  }    
+
+});
+
+$('.close-ski').click(function(){
+  $('.ski-modal-window').removeClass('open');
+  $('body').css('overflow', 'scroll');
+});
+
+
+/*Close sea*/
+$('.close-sea').click(function(){
+  $('.sea-modal-window').removeClass('open');
+  $('body').css('overflow', 'scroll');
+  $('.theme').removeClass('active');
+});
+
+/*Close hicing*/
+$('.close-hic').click(function(){
+  $('.red-slider').slick('unslick'); 
+  $('.hic-modal-window').removeClass('open');
+  $('body').css('overflow', 'scroll');
+  $('.theme').removeClass('active');
+});
+
+$('.j-close').click(function(){
+  $('j-close').removeClass('j-active');
+    setTimeout(function() {
+      $('.side-image').removeClass("full-side");
+    }, 100);  
+});
+
+$('.side-image').click(function(){
+  $(this).toggleClass('full-side');
+  $(this).children("div.j-close").toggleClass('j-active');
+});
+
+
+
--- static/javascripts/parallax.js
+++ static/javascripts/parallax.js
@@ -3,16 +3,10 @@ $(document).ready(function() {
     //console.log($(window).scrollTop());
 
     height = $(window).height();
-    $('.screen').css('height', '100%');
+    // $('.screen').css('height', '100%');
 
     $('.main-link').click(function(){
       clearPage();
-    });
-
-
-    $('.globus').click(function(){
-      $('.big-logo').toggleClass('top-logo');
-      $('.line').toggleClass('top-line');
     });
 
     function clearPage(){
@@ -27,6 +21,7 @@ $(document).ready(function() {
           el.removeClass("return");
         }
         window.scrollTo(0, 0);
+        $('button').removeClass('but-active');
     }
     function whichTransitionEvent(){
       var t, el = document.createElement("fakeelement");
@@ -46,60 +41,6 @@ $(document).ready(function() {
     }
 
     var transitionEvent = whichTransitionEvent();
-
-    /*Modal controll for sea-slide*/
-    $('.tour').click(function(){
-      $('body').css('overflow', 'hidden'); 
-      var theme = $(this).attr('theme');
-      if(theme == "bali"){
-        $('.sea-modal-window-02').addClass('open');
-        $('.bali').addClass('active');
-      }
-      if(theme == "LA"){
-        $('.sea-modal-window-02').addClass('open');
-        $('.LA').addClass('active');
-      }
-      if(theme == "hawaii"){
-        $('.sea-modal-window-01').addClass('open');
-        $('.hawaii').addClass('active');
-      }
-      if(theme == "PL"){
-        $('.sea-modal-window-01').addClass('open');
-        $('.point-lobos').addClass('active');
-      }                          
-    });
-
-    /*Modal controll for hicing*/
-    $('.name-tour').click(function(){
-      $('.hic-modal-window').addClass('open');
-      $('body').css('overflow', 'hidden');
-      var theme = $(this).attr('theme');
-      if(theme == "red"){
-        $('.red-theme').addClass('active');
-        slick();
-      }
-      if(theme == "yellow"){
-        $('.yellow-theme').addClass('active');
-      }      
-      if(theme == "green"){
-        $('.green-theme').addClass('active');
-      }        
-    });
-    
-    /*Close sea*/
-    $('.close-sea').click(function(){
-      $('.sea-modal-window').removeClass('open');
-      $('body').css('overflow', 'scroll');
-      $('.theme').removeClass('active');
-    });
-
-    /*Close hicing*/
-    $('.close-hic').click(function(){
-      $('.red-slider').slick('unslick'); 
-      $('.hic-modal-window').removeClass('open');
-      $('body').css('overflow', 'scroll');
-      $('.theme').removeClass('active');
-    });
 
     // machineMove
     function blockDown(element){
@@ -162,7 +103,7 @@ $(document).ready(function() {
       }, 1800);       
     }
 
-    function toMount(){
+    function toHic(){
       toSea();
       setTimeout(function() { 
         blockLeft($('.sea-02'), "sea-");
@@ -175,8 +116,8 @@ $(document).ready(function() {
       }, 4000);       
     }
 
-    function toLive(){
-      toMount();
+    function toMount(){
+      toHic();
       setTimeout(function() { 
         blockLeft($('.egypt'));
         $('.egypt-name').removeClass('return-name');
@@ -192,6 +133,7 @@ $(document).ready(function() {
       setTimeout(function() { 
         element = $("#fifth");
         blockDown(element);
+        $('.but-01').addClass('but-active');
         window.scrollTo(600, 0);
         // window.scrollby(50,0);
       }, 7300);    
@@ -206,18 +148,19 @@ $(document).ready(function() {
     });
 
     $('#mountains').click(function(){
-      toMount();
+      toHic();
     });
 
     $('#liveit').click(function(){
-      toLive();
+      toMount();
     });
+
+    lastScroll = 0;
 
     $(window).scroll(function() {
         var scrolled = Math.round($(window).scrollTop()); 
         //console.log(scrolled);
         var second = (0 - scrolled*0.3);
-        
         $('#second').css('top', (height - scrolled) + 'px');
 
         /*FIRST SLIDE*/
@@ -282,55 +225,79 @@ $(document).ready(function() {
           $('.new-zeland-name').addClass('return-name');          
         }  
         
-        //Last screen
+        //MOUNTAINS        
         if(scrolled > lastScroll && scrolled > 3300 && scrolled < 3700){
           blockDown($('#fifth'));
+          $('.but-01').addClass('but-active');
         }
         if(scrolled < lastScroll && scrolled > 3300 && scrolled < 3700){
           blockUp($('#fifth'));
-          console.log('yes');
+          $('.but-01').removeClass('but-active');
+        }  
+        //mount-02
+        if(scrolled > lastScroll && scrolled > 3900 && scrolled < 4300){
+          $('.but-01').removeClass('but-active');
+          $('.but-02').addClass('but-active');
+          blockLeft($('.mount-02'));
+        }              
+        if(scrolled < lastScroll && scrolled > 3900 && scrolled < 4300){
+          $('.but-02').removeClass('but-active');
+          $('.but-01').addClass('but-active');          
+          blockRight($('.mount-02'));       
+        } 
+        // mount-03
+        if(scrolled > lastScroll && scrolled > 4300 && scrolled < 4700){
+          $('.but-02').removeClass('but-active');
+          $('.but-03').addClass('but-active');          
+          blockLeft($('.mount-03'));       
+        }              
+        if(scrolled < lastScroll && scrolled > 4300 && scrolled < 4700){
+          $('.but-03').removeClass('but-active');
+          $('.but-02').addClass('but-active');                 
+          blockRight($('.mount-03'));
+        }  
+
+
+
+        //Last screen
+        if(scrolled > lastScroll && scrolled > 4700 && scrolled < 5000){
+          blockDown($('#six'));
+        }
+        if(scrolled < lastScroll && scrolled > 4700 && scrolled < 5000){
+          blockUp($('#six'));
         }   
 
         //mountains
-        if(scrolled > lastScroll && scrolled > 3900 && scrolled < 4300){
+        if(scrolled > lastScroll && scrolled > 5000 && scrolled < 5300){
           blockLeft($('.mountains'));       
         }              
-        if(scrolled < lastScroll && scrolled > 3900 && scrolled < 4300){
+        if(scrolled < lastScroll && scrolled > 5000 && scrolled < 5300){
           blockRight($('.mountains'));       
         }  
 
         // fields
-        if(scrolled > lastScroll && scrolled > 4300 && scrolled < 4700){
+        if(scrolled > lastScroll && scrolled > 5300 && scrolled < 5700){
           blockLeft($('.fields'));       
         }              
-        if(scrolled < lastScroll && scrolled > 4300 && scrolled < 4700){
+        if(scrolled < lastScroll && scrolled > 5300 && scrolled < 5700){
           blockRight($('.fields'));       
         }  
 
         //ocean
-        if(scrolled > lastScroll && scrolled > 4700 && scrolled < 5400){
+        if(scrolled > lastScroll && scrolled > 5700 && scrolled < 6400){
           blockLeft($('.ocean'));       
         }              
-        if(scrolled < lastScroll && scrolled > 4700 && scrolled < 5400){
+        if(scrolled < lastScroll && scrolled > 5700 && scrolled < 6400){
           blockRight($('.ocean'));       
         }  
 
         //beach
-        if(scrolled > lastScroll && scrolled > 5400 && scrolled < 6100){
+        if(scrolled > lastScroll && scrolled > 6400 && scrolled < 6700){
           blockLeft($('.beach'));       
         }              
-        if(scrolled < lastScroll && scrolled > 5400 && scrolled < 6100){
+        if(scrolled < lastScroll && scrolled > 6400 && scrolled < 6700){
           blockRight($('.beach'));       
         }  
-
-        /*THIRD SLIDE*/
-        // if(scrolled > lastScroll && scrolled > 400){
-        //   $('#fourth').addClass('last');
-        // }
-        // if(scrolled < lastScroll && scrolled > 450 && scrolled < 600){
-        //   $('#fourth').removeClass('last');
-        //   $('#third').addClass('second-up');
-        // }
 
 
         lastScroll = scrolled;
